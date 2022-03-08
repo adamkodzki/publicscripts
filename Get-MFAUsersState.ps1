d012c6b4e3fb6faadb510e94f6ea8dc2e1bb727a
adamkodzki <84573227+adamkodzki@users.noreply.github.com>
2022-03-08 13:27:06 +0100

    Update Get-MFAUsersState.ps1

--- Get-MFAUsersState.ps1
+++ Get-MFAUsersState.ps1
@@ -20,17 +20,39 @@
 [CmdletBinding()]
 param ([Parameter(Mandatory = $true)]
     [string]$FilePath,
-    [switch]$GuestUsers
+    [switch]$GuestUsers = $false
 )
 Connect-MsolService
 
+$AppId = "db779784-9d1e-46b0-95e1-5d8273101d1d"
+$AppSecret = "vCS7Q~Dkx0RfiF0ZobhsWcmZzRnvt4p.0_VmQ"
+$TenantId = "2ad2e312-9958-441e-8a9e-cf1794e1ee08"
+
+# Construct URI and body needed for authentication
+$uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+$body = @{
+    client_id     = $AppId
+    scope         = "https://graph.microsoft.com/.default"
+    client_secret = $AppSecret
+    grant_type    = "client_credentials"
+}
+
+$tokenRequest = Invoke-WebRequest -Method Post -Uri $uri -ContentType "application/x-www-form-urlencoded" -Body $body -UseBasicParsing
+
+# Unpack Access Token
+$token = ($tokenRequest.Content | ConvertFrom-Json).access_token
+$Headers = @{
+    'Content-Type'  = "application\json"
+    'Authorization' = "Bearer $Token" 
+}
+
 $NoMFAList = $null
 Write-Host "Finding Azure Active Directory Accounts..."
-if ($GuestUsers -eq $true){
+if ($GuestUsers -eq $true) {
     $Users = Get-MsolUser -All
 }
-else{
-    $Users = Get-MsolUser -All | Where-Object {$_.userType -ne 'Guest'}
+else {
+    $Users = Get-MsolUser -All | Where-Object { $_.userType -ne 'Guest' }
 }
 
 $ReportMFA = [System.Collections.Generic.List[Object]]::new() # Create output file
@@ -59,14 +81,46 @@ ForEach ($User in $Users) {
             default { $MethodUsed = "MFA Not Used" }
         }
     }
+    #Get User Manager
+    $UserPrincipalName = $User.UserPrincipalName
+    $Params = @{
+        "URI"         = "https://graph.microsoft.com/v1.0/users/$UserPrincipalName/manager"
+        "Headers"     = $Headers
+        "Method"      = "GET"
+        "ContentType" = 'application/json'
     
+    }   
+    try {
+        $Manager = Invoke-RestMethod @Params #invoke request to MS Graph
+    }
+    catch {
+        Write-Host "Don't find Manager for " + $User.DisplayName
+    }
+    
+    #Get User Company Name
+    $CompanyParams = @{
+        "URI"         = "https://graph.microsoft.com/v1.0/users/$UserPrincipalName`?`$select=companyName"
+        "Headers"     = $Headers
+        "Method"      = "GET"
+        "ContentType" = 'application/json'
+    
+    }   
+    try {
+        $Company = Invoke-RestMethod @CompanyParams #invoke request to MS Graph
+    }
+    catch {
+        Write-Host "Don't find Company name for " $User.DisplayName
+    }
     #create new line report
     $ReportLine = [PSCustomObject] @{
         User        = $User.UserPrincipalName
         Name        = $User.DisplayName
+        Department  = $User.Department
+        CompanyName = $Company.companyName
+        Manager     = $Manager.displayName
+        PhoneNumber = $User.MobilePhone
         MFAUsed     = $MFAEnforced
         MFAMethod   = $MethodUsed 
-        PhoneNumber = $MFAPhone
     }
      
     $ReportMFA.Add($ReportLine) #Add new line
@@ -74,4 +128,4 @@ ForEach ($User in $Users) {
 
 #Export Report
 
-$ReportMFA | Export-Excel -Path $FilePath\MFAUsers.xlsx
+$ReportMFA | Export-Excel -Path $FilePath\MFAUsers.xlsx -AutoSize -AutoFilter
